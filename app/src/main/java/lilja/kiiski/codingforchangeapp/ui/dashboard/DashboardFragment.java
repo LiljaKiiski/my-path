e60bcd8afc7943d6720223ecad1a315ee63014fb
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-08-05 16:11:27 -0700

    basic graph (non mood)

--- app/src/main/java/lilja/kiiski/codingforchangeapp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/lilja/kiiski/codingforchangeapp/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package lilja.kiiski.codingforchangeapp.ui.dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +11,32 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import lilja.kiiski.codingforchangeapp.R;
 import lilja.kiiski.codingforchangeapp.databinding.FragmentDashboardBinding;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+
+    // variable for our bar chart
+    BarChart barChart;
+
+    // variable for our bar data.
+    BarData barData;
+
+    // variable for our bar data set.
+    BarDataSet barDataSet;
+
+    // array list for storing entries.
+    ArrayList barEntriesArrayList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +45,38 @@ public class DashboardFragment extends Fragment {
         View root = binding.getRoot();
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        barChart = view.findViewById(R.id.idBarChart);
+        getBarEntries();
+        barDataSet = new BarDataSet(barEntriesArrayList, "Geeks for Geeks");
+        barData = new BarData(barDataSet);
+        barChart.setData(barData);
+
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+
+        barDataSet.setValueTextSize(16f);
+        barChart.getDescription().setEnabled(false);
+
+    }
+
+    private void getBarEntries() {
+        // creating a new array list
+        barEntriesArrayList = new ArrayList<>();
+
+        // adding new entry to our array list with bar
+        // entry and passing x and y axis value to it.
+        barEntriesArrayList.add(new BarEntry(1f, 4));
+        barEntriesArrayList.add(new BarEntry(2f, 6));
+        barEntriesArrayList.add(new BarEntry(3f, 8));
+        barEntriesArrayList.add(new BarEntry(4f, 2));
+        barEntriesArrayList.add(new BarEntry(5f, 4));
+        barEntriesArrayList.add(new BarEntry(6f, 1));
     }
 
     @Override
