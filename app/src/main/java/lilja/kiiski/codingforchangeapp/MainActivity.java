6d2b66375e46b797ca81fdd38cf303d04c16b84f
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-08-05 17:38:32 -0700

    need to make "my path" view button + help page

--- app/src/main/java/lilja/kiiski/codingforchangeapp/MainActivity.java
+++ app/src/main/java/lilja/kiiski/codingforchangeapp/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_help)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
