76fb599c0c7a25eca47a68a3555f5b7d2fa951b9
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-08-05 20:23:15 -0700

    perhaps almost done?

--- app/src/main/java/lilja/kiiski/codingforchangeapp/PathActivity.java
+++ app/src/main/java/lilja/kiiski/codingforchangeapp/PathActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.util.Locale;
 
@@ -37,8 +38,34 @@ public class PathActivity extends AppCompatActivity {
         days_on_journey = (TextView) findViewById(R.id.num_days);
         start_date = (TextView) findViewById(R.id.start_journey);
 
+        Date c = Calendar.getInstance().getTime();
+        System.out.println("Current time => " + c);
+
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        String formattedDate = df.format(c);
+
         growth_path.setText("You are on your path to " + settings.getString("user_path", "null"));
+        start_date.setText("You started your journey on " + settings.getString("start_date", formattedDate));
 
+        String CurrentDate= settings.getString("start_date", formattedDate);
+        String FinalDate= formattedDate;
+        Date date1;
+        Date date2;
+        SimpleDateFormat dates = new SimpleDateFormat("MM/dd/yyyy");
+        try {
+            date1 = dates.parse(CurrentDate);
+            date2 = dates.parse(FinalDate);
+            long difference = Math.abs(date1.getTime() - date2.getTime());
+            long differenceDates = difference / (24 * 60 * 60 * 1000) + 1;
 
+            if (differenceDates > 1) {
+                days_on_journey.setText("You have been on your journey " + differenceDates + " days");
+            } else {
+                days_on_journey.setText("You have been on your journey " + differenceDates + " day");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.out.println("EXCEPTION");
+        }
     }
 }
